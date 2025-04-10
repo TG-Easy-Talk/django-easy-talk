2bd502a15e903bd5ae7f400a1e37a141431e9e0d
Felipe de Carvalho Santos <142360153+Felipinho5@users.noreply.github.com>
2025-04-10 11:07:28 -0300

    Revert "Pasta static global renomeada para globalstatic, setting LANGUAGE_CODE mudou para pt-br"
    This reverts commit d3041608d80429961045379925fbe8adf1191618.

--- easy_talk/settings.py
+++ easy_talk/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'globalstatic',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
