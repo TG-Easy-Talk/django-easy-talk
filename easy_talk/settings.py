d3041608d80429961045379925fbe8adf1191618
Felipe de Carvalho Santos <142360153+Felipinho5@users.noreply.github.com>
2025-04-10 10:51:08 -0300

    Pasta static global renomeada para globalstatic, setting LANGUAGE_CODE mudou para pt-br

--- easy_talk/settings.py
+++ easy_talk/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'globalstatic',
 ]
 
 # Default primary key field type
