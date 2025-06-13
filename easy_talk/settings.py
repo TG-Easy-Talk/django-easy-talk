cd73e873bdff2d31d14cd9bfce0f88c83eec66e7
vinicius <andrade.vinicius@ifood.com.br>
2025-06-13 19:26:37 -0300

    deploy: update requirements and configure static/media file settings

--- easy_talk/settings.py
+++ easy_talk/settings.py
@@ -106,14 +106,15 @@ USE_TZ = True
 # Static and media files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+# Media files
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
