30715449077bdfe63d0ec9798b03ca881d208590
vinicius <andrade.vinicius@ifood.com.br>
2025-06-13 21:40:17 -0300

    deploy: update ALLOWED_HOSTS to include new IP address

--- easy_talk/settings.py
+++ easy_talk/settings.py
@@ -10,10 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$c=8t#fntxw=r42q0#^9avu-l)m@9x3iigcf^+k9)(v!li^j$3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [
+    '98.84.189.25'
     '184.72.118.136',
     'localhost',
     '127.0.0.1',
